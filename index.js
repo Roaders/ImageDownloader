78bd86b9d1b3feacfea7e67e1448538b698a46d1
Giles Roadnight <giles@roadnight.name>
2016-10-23 10:21:26 +0100

    single file downloaded, rx friendly

--- index.js
+++ index.js
@@ -18,14 +18,6 @@ var regExp = new RegExp(pattern,"g");
 
 var cookie = fs.readFileSync( "cookie.txt" );
 
-console.log("Creating output folder");
-
-fs.mkdirSync("output");
-
-console.log("Loading Webpage: " + webpageUrl);
-
-console.log("Cookie content: " + cookie);
-
 function getUrl(targetUrl){
 	var urlObject = url.parse(targetUrl);
 
@@ -39,47 +31,72 @@ function getUrl(targetUrl){
 		}
 	};
 
-	return http.get(options);
+	return Rx.Observable.fromEvent( http.get(options), "response" )
+		.take(1);
 }
-Rx.Observable.fromEvent( getUrl(webpageUrl), "response" )
-	.take(1)
-	.flatMap(function(response){
-		console.log("handling response code: " + response.statusCode);
-		
-		return Rx.Observable.fromEvent(response, "data")
-			.takeUntil( Rx.Observable.fromEvent(response, "end") );
-	})
-	.toArray()
-	.map(function(allData){
-		return allData.join("");
-	})
-	.flatMap( function(htmlContent){
-		
-		console.log( "Searching for images using " + pattern );
-		
-		return Rx.Observable.from(htmlContent.match(regExp));
-	})
-	.map(function(regExMatch){
-		var regExResults = regExp.exec(regExMatch);
-		regExp.lastIndex = 0;
-		
-		return imageUrl = regExResults[regExResults.length-1];
-	})
-	.take(1)
-	.do( function(imageUrl){
-		
-		var imagePath = path.parse(imageUrl);
-		
-		console.log( "saving image " + imageUrl + " (" + imagePath.name + imagePath.ext + ")" );
-		
-		var file = fs.createWriteStream("output/" + imagePath.name + imagePath.ext );
-		var request = http.get(imageUrl, function(response) {
-			response.pipe(file);
-		});
 
-	} )
-	.subscribe( function(item){
-		//console.log("item: " + item );
-	},null,function(){
-		console.log("complete");
-	});
+function loadImages(){
+
+	console.log("Loading Webpage: " + webpageUrl);
+
+	console.log("Cookie content being added");
+
+	getUrl(webpageUrl)
+		.flatMap(function(response){
+			console.log("handling response code: " + response.statusCode);
+			
+			return Rx.Observable.fromEvent(response, "data")
+				.takeUntil( Rx.Observable.fromEvent(response, "end") );
+		})
+		.toArray()
+		.map(function(allData){
+			return allData.join("");
+		})
+		.flatMap( function(htmlContent){
+			
+			console.log( "Searching for images using " + pattern );
+			
+			var matches = htmlContent.match(regExp);
+
+			console.log(matches.length + " matches found");
+
+			return Rx.Observable.from(matches);
+		})
+		.map(function(regExMatch){
+			var regExResults = regExp.exec(regExMatch);
+			regExp.lastIndex = 0;
+			
+			return imageUrl = regExResults[regExResults.length-1];
+		})
+		.take(1)
+		.flatMap( function(imageUrl){
+			
+			var imagePath = path.parse(imageUrl);
+			
+			console.log( "saving image " + imageUrl + " (" + imagePath.name + imagePath.ext + ")" );
+			
+			var fileStream = fs.createWriteStream("output/" + imagePath.name + imagePath.ext );
+			var request = http.get(imageUrl, function(response) {
+				response.pipe(fileStream);
+			});
+
+			return Rx.Observable.fromEvent(fileStream, "finish")
+				.take(1);
+
+		} )
+		.subscribe( function(item){
+			//console.log("item: " + item );
+		},null,function(){
+			console.log("complete");
+		});
+}
+
+fs.lstat('output', function(err, stats) {
+
+    if (!stats || !stats.isDirectory()) {
+		console.log("Creating output folder");
+        fs.mkdirSync("output");
+    } 
+
+	loadImages();
+});
